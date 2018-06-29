c01749c7ffa657ddc977e7c7ed5ad82fb72e0af6
alphaseinor <alphaseinor@gmail.com>
2018-06-29 18:16:07 -0500

    added keys file example, and server.js with connection to mongoose and mlab

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+//mlab.com URI link, using AWS sandbox, makes new variable db for connecting
+const db = require('./config/keys').mongoURI;
+//connect to DB
+mongoose
+    .connect(db)
+    .then(() => console.log('connected to db... ')) //if it connects then
+    .catch(err => console.log(err)); // if it doesn't connect then
+
+const port = process.env.PORT || 5000; // this allows us to go serverless || local
+
+app.listen(port, () => console.log(`server started on port ${port}`)); //listen on above port
